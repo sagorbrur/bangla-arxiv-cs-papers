767929f060b72100514013b609c95f981c147bdb
Sagor Sarker <sagorhem3532@gmail.com>
2021-01-25 20:09:16 +0600

    Update query_arxiv.py

--- query_arxiv.py
+++ query_arxiv.py
@@ -6,7 +6,7 @@ def query_arxiv_by_keywords(query_texts_list, output_json_name="output_papers.js
   total_papers = []
   for query_text in tqdm(query_texts_list):
       result = arxiv.query(
-        query="bangla",
+        query=query_text,
         max_chunk_results=10,
         max_results=max_results,
         iterative=True
